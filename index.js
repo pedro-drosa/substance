afc080a7009dc043a6dcf1ec9e83290e4ab974d3
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-02 10:13:51 -0300

    feat(index.js): fetch_all

--- index.js
+++ index.js
@@ -22,3 +22,33 @@ function create_article() {
 
   articles.push(article);
 }
+
+function fetch_all() {
+  articles.forEach((elem) => {
+    let section = document.querySelector("[data-section]");
+    let article_dom = document.createElement("article");
+    let title_dom = document.createElement("h2");
+    let content_dom = document.createElement("p");
+    let date_dom = document.createElement("span");
+    let edit_button = document.createElement("button");
+    let delete_button = document.createElement("button");
+
+    title_dom.innerHTML = elem.title;
+    content_dom.innerHTML = elem.content;
+    date_dom.innerHTML =
+      elem.updated_at == null ? elem.created_at : elem.updated_at;
+    edit_button.innerHTML = "Edit";
+    delete_button.innerHTML = "Delete";
+
+    article_dom.appendChild(title_dom);
+    article_dom.appendChild(content_dom);
+    article_dom.appendChild(date_dom);
+    article_dom.appendChild(edit_button);
+    article_dom.appendChild(delete_button);
+
+    article_dom.setAttribute("class", "articles__article");
+    edit_button.setAttribute("class", "button__form button__form--default");
+    delete_button.setAttribute("class", "button__form button__form--danger");
+    section.appendChild(article_dom);
+  });
+}
