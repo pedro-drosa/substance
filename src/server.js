1f487c7efa0df732d280a21313e6e4f135d4be59
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-09 19:43:05 -0300

    chore: add docker and docker-compose

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import http from 'http';
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello Docker');
 });
 
 server.listen(port, hostname, () => {
