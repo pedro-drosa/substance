775ddf48ab4a6e112e1d3f54fb549c8d507181bc
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-12 14:51:39 -0300

    feat: add migration and user model

---
+++ src/app/models/User.js
@@ -0,0 +1,24 @@
+import { Model, DataTypes } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+class User extends Model {
+  static init(connection) {
+    super.init(
+      {
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+      },
+      { sequelize: connection, tableName: 'users', freezeTableName: 'users' },
+    );
+
+    this.addHook('beforeCreate', async (user) => {
+      const currentUser = user || undefined;
+      currentUser.password = await bcryptjs.hash(user.password, 10);
+      return user;
+    });
+  }
+}
+
+export default User;
---
+++ src/database/migrations/20220912151922-create-users.js
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
