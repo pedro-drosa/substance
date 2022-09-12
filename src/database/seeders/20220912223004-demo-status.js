ceff9ec4f320e86c42f66716f9c34e819a0a6394
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-12 19:48:58 -0300

    feat: add seeders in status table

---
+++ src/database/seeders/20220912223004-demo-status.js
@@ -0,0 +1,28 @@
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('status', [
+      {
+        id: 1,
+        type: 'approvado',
+        createdAt: '2022-09-12 00:0:00',
+        updatedAt: '2022-09-12 00:0:00',
+      },
+      {
+        id: 2,
+        type: 'em revisão',
+        createdAt: '2022-09-12 00:00:00',
+        updatedAt: '2022-09-12 00:0:00',
+      },
+      {
+        id: 3,
+        type: 'reprovado',
+        createdAt: '2022-09-12 00:00:00',
+        updatedAt: '2022-09-12 00:0:00',
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('status', null, {});
+  },
+};
