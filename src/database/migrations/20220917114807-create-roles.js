bfb1b6b6ef0658f90c08d5b9fa7081c35cea99df
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-17 09:10:17 -0300

    feat: add role migration

---
+++ src/database/migrations/20220917114807-create-roles.js
@@ -0,0 +1,34 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('roles', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('roles');
+  },
+};
