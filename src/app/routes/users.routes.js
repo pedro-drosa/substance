2fea594143785aab9f4830408972f43097704db8
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-19 21:21:47 -0300

    fix: remove authentication middleware

--- src/app/routes/users.routes.js
+++ src/app/routes/users.routes.js
@@ -1,16 +1,10 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import ensureensureAuthenticated from '../middlewares/ensureAuthenticated';
-import ensureAdminRole from '../middlewares/ensureAdminRole';
 
 const userRouter = Router();
 
-userRouter.post(
-  '/',
-  ensureensureAuthenticated,
-  ensureAdminRole,
-  UserController.store,
-);
+userRouter.post('/', UserController.store);
 userRouter.get('/', ensureensureAuthenticated, UserController.index);
 userRouter.get('/:id', ensureensureAuthenticated, UserController.get);
 userRouter.put('/:id', UserController.update);
