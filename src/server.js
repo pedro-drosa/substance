643fabe8a8f4856e3e6678fd345625b8b8656ee1
pedro-drosa <pedro_drosa@hotmail.com>
2022-09-22 21:17:20 -0300

    style: change log message

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import App from './App';
 
-const APP_PORT = 5000;
-const APP_BASE_URL = 'http://localhost';
+const APP_PORT = process.env.PORT || process.env.APP_PORT;
+const { APP_BASE_URL } = process.env;
 
 App.listen(APP_PORT, () => {
-  console.log(`Servidor rodando em:${APP_BASE_URL}:${APP_PORT}`);
+  console.log(`server running on:${APP_BASE_URL}:${APP_PORT}`);
 });
